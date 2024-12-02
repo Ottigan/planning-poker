d08498905bd2f31ddafbe10af2923c978d15a543
Janis Malcans <jmalcans@evolution.com>
2024-12-02 10:08:54 +0200

    Test automatic deploy for main

--- index.go
+++ index.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+		fmt.Fprintf(w, "Hello, Agile Zombies!")
 	})
 
 	fmt.Println("Server is running on port 8080")
